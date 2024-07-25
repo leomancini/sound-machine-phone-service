0ac6e128093348e56e6196b22ff73b440d9441cd
Leo Mancini <mancini@leo.gd>
2024-07-24 22:23:21 -0400

    updating flow

--- server.js
+++ server.js
@@ -13,28 +13,51 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/ivr", (req, res) => {
   const twiml = new VoiceResponse();
+  const digits = req.body.Digits || req.body.SpeechResult;
 
-  const digits = req.body.Digits;
+  const handleInput = () => {
+    twiml.gather({
+      input: "dtmf speech",
+      action: "/ivr",
+      method: "POST",
+      speechTimeout: "auto",
+      speechModel: "numbers_and_commands",
+    });
+  };
 
-  if (digits) {
+  const processInput = (input) => {
     const selectedOption = config.options.find(
-      (option) => option.digit === digits
+      (option) =>
+        option.digit === input || option.spokenWord === input.toLowerCase()
     );
     if (selectedOption) {
       twiml.say(selectedOption.message);
       twiml.play(
+        {
+          loop: 1,
+        },
         `${config.audioBaseUrl}${selectedOption.audioId}${config.audioFilePath}`
       );
+      handleInput(); // Allow input after audio message
     } else {
       twiml.say(config.invalidInputMessage);
+      handleInput();
     }
+  };
+
+  if (digits) {
+    processInput(digits);
   } else {
-    twiml.say(config.welcomeMessage);
-    twiml.gather({
-      numDigits: 1,
-      action: "/ivr",
-      method: "POST",
-    });
+    twiml.say(
+      {
+        voice: "alice",
+        language: "en-US",
+        input: "speech dtmf",
+        interruptible: true,
+      },
+      config.welcomeMessage
+    );
+    handleInput();
   }
 
   res.type("text/xml");
