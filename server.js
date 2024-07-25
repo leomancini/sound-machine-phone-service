89852e6b72ae34f057d65c76fed4a8f48ca02d38
Leo Mancini <mancini@leo.gd>
2024-07-24 21:57:38 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3100;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log('Server is running');
+});
+
